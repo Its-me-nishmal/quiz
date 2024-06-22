3a575f536f8edfd4b02b64819cabe974049b3f97
alltrackerx <alltrackerx@gmail.com>
2024-06-22 09:20:10 +0000

    ok

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,6 @@ const keys = require('../config/keys');
 exports.registerUser = [
   // Validation and sanitization
   check('name', 'Name is required').not().isEmpty(),
-  check('username', 'Username is required').not().isEmpty(),
   check('email', 'Please include a valid email').isEmail(),
   check('password', 'Password must be at least 6 characters').isLength({ min: 6 }),
   async (req, res) => {
